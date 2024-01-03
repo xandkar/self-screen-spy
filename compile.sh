b0db1e993a76f26368c674a95f5128357a34fa3d
Siraaj Khandkar <siraaj@khandkar.net>
2024-01-02 21:00:46 -0500

    Make framerate configurable

--- compile.sh
+++ compile.sh
@@ -4,6 +4,7 @@ set -e
 
 main() {
     local -r data_dir_path=${1:-data}
+    local -r framerate=${2:-5}
     local -r snapshot_seq_pattern='%09d'
     local -r snapshot_file_ext='jpeg'
 
@@ -18,7 +19,7 @@ main() {
             # printf '[debug] %s ---> %s\n' "$snapshot_file_path" "$seq_file_path" >&2
             cp "$snapshot_file_path" "$seq_file_path"
         done < <(find "$day_data_dir_path" -mindepth 1 -maxdepth 1 -type f -iname "*.${snapshot_file_ext}" | sort)
-        ffmpeg -y -nostdin -framerate 24 -i "$temp_dir_path"/"$snapshot_seq_pattern"."$snapshot_file_ext" -c:v libx264 -pix_fmt yuv420p "$day".mp4
+        ffmpeg -y -nostdin -framerate "$framerate" -i "$temp_dir_path"/"$snapshot_seq_pattern"."$snapshot_file_ext" -c:v libx264 -pix_fmt yuv420p "$day".mp4
     done < <(find "$data_dir_path" -mindepth 1 -maxdepth 1 -type d | sort)
 }
 
